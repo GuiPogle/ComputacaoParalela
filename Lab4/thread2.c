9d1fb0acfd29c12f2dd564f1110ae455473907d3
Laura C. Balbachan dos Santos <84653771+Balbachan@users.noreply.github.com>
2023-04-18 23:24:47 -0300

    Create thread2.c

---
+++ Lab4/thread2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <pthread.h>
+
+// define a matriz 
+#define NUM_THREADS 3
+int matriz[3][3] = {{1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9}};
+int vetor[3] = {2, 3, 4};
+int resultado[3] = {0, 0, 0};
+
+// função que separa uma linha para cada thread multiplicar
+void *multiplicacao(void *thread_id) {
+    int id = *((int*) thread_id); // extrai o ID da thread
+    int i;
+    for (i = 0; i < 3; i++) {
+        resultado[id] += matriz[id][i] * vetor[i];
+    }
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t threads[NUM_THREADS];
+    int t;
+    int thread_ids[NUM_THREADS];
+
+    // cria as threads
+    for (t = 0; t < NUM_THREADS; t++) {
+        thread_ids[t] = t;
+        int rc = pthread_create(&threads[t], NULL, multiplicacao, (void*) &thread_ids[t]);
+        if (rc) {
+            printf("Erro ao criar a thread %d\n", t);
+            return -1;
+        }
+    }
+
+    // espera as threads terminarem
+    for (t = 0; t < NUM_THREADS; t++) {
+        pthread_join(threads[t], NULL);
+    }
+
+    // imprime o resultadodf
+    printf("Resultado (threads): ");
+    int i;
+    for (i = 0; i < 3; i++) {
+        printf("%d ", resultado[i]);
+    }
+    printf("\n");
+
+    pthread_exit(NULL);
+}
