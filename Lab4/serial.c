d748565e67df39b4e64f8e0f7909311cb9bcdf07
Laura C. Balbachan dos Santos <84653771+Balbachan@users.noreply.github.com>
2023-04-18 23:22:27 -0300

    Update serial.c

--- Lab4/serial.c
+++ Lab4/serial.c
@@ -6,14 +6,16 @@ int main() {
     int matriz[3][3] = {{1, 2, 3},
                         {4, 5, 6},
                         {7, 8, 9}};
-    int vetor[3] = {2, 3, 4};
+    int vetor[3][3] = {{1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}};
     int resultado[3] = {0, 0, 0};
     int i, j;
 
     // multiplica a matriz por vetor
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
-            resultado[i] += matriz[i][j] * vetor[j];
+            resultado[i] += matriz[i][j] * vetor[i][j];
         }
     }
 
