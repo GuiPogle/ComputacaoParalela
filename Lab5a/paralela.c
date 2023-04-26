341e2cf70fbd6470902ad6195464cfe286516f54
Laura C. Balbachan dos Santos <84653771+Balbachan@users.noreply.github.com>
2023-04-25 22:47:32 -0300

    correções de sintaxe

--- Lab5a/paralela.c
+++ Lab5a/paralela.c
@@ -29,10 +29,9 @@ void Trap(double a, double b, int n, double *global_result_p) {
   double local_a, local_b;
   int i, local_n;
   int my_rank = omp_get_thread_num();
-  int thread_count = omp_get_num_threads()
-      :
+  int thread_count = omp_get_num_threads():
 
-        h = (b - a) / n;
+  h = (b - a) / n;
   local_n = n / thread_count;
   local_a = a + my_rank * local_n * h;
   local_b = local_a + local_n * h;
