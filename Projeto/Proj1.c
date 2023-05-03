bf4dd8d00f577018657f58f5e4e54e76874b2b99
Ubuntu <ubuntu@ip-172-31-2-40.ec2.internal>
2023-05-03 02:57:30 +0000

    Pastas

--- Projeto/Proj1.c
+++ Projeto/Proj1.c
@@ -5,18 +5,17 @@ int main(void)
 {
     mpf_t e, temp, fat;
 
-    mpf_init2(e, 100000);
-    mpf_init2(temp, 100000);
-    mpf_init2(fat, 100000);
+    mpf_init2(e, 10000);
+    mpf_init2(temp, 64);
+    mpf_init2(fat, 64);
 
     mpf_set_d(e, 1.0);
-    mpf_set_d(temp, 1.0);
-    mpf_set_d(fat, 1.0);
+    mpf_set_ui(temp, 1);
+    mpf_set_ui(fat, 1);
 
-    for(int i = 1; i <= 1000000; i++)
+    for(int i = 1; i <= 10000; i++)
     {
         mpf_mul_ui(fat, fat, i);
-        mpf_set_ui(temp, 1);
         mpf_div(temp, temp, fat);
         mpf_add(e, e, temp);
     }
@@ -24,4 +23,3 @@ int main(void)
     gmp_printf("%.*Ff", 10000, e);
     return 0;
 }
-
