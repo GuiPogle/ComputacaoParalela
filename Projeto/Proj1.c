16b6539ec28ecd6cb530bd05971afd8b215cd546
Ubuntu <ubuntu@ip-172-31-2-40.ec2.internal>
2023-05-09 13:02:19 +0000

    Precisão ajustada

--- Projeto/Proj1.c
+++ Projeto/Proj1.c
@@ -3,23 +3,24 @@
 
 int main(void) 
 {
+    mpf_set_default_prec(100000);
     mpf_t e, temp, fat;
 
-    mpf_init2(e, 10000);
-    mpf_init2(temp, 64);
-    mpf_init2(fat, 64);
+    mpf_init2(e);
+    mpf_init2(temp);
+    mpf_init2(fat);
 
     mpf_set_d(e, 1.0);
     mpf_set_ui(temp, 1);
     mpf_set_ui(fat, 1);
 
-    for(int i = 1; i <= 10000; i++)
+    for(int i = 1; i <=20000; i++)
     {
         mpf_mul_ui(fat, fat, i);
-        mpf_div(temp, temp, fat);
+        mpf_div(temp, 1, fat);
         mpf_add(e, e, temp);
     }
 
-    gmp_printf("%.*Ff", 10000, e);
+    gmp_printf("%.*Ff", 20000, e);
     return 0;
 }
