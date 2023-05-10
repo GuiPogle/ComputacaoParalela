59ddba9c9dfba6d5fc68749edbcec77299796de1
GuiPogle <guilhermevalente.profissional@gmail.com>
2023-05-10 05:34:45 +0000

    Projeto 2 - Versao Paralela (editar)

--- Projeto/Proj1.c
+++ Projeto/Proj1.c
@@ -1,55 +1,28 @@
 #include <stdio.h>
 #include <gmp.h>
 
-int main(void) 
+int main()
 {
-<<<<<<< HEAD
-    mpf_t e, temp, fat;
+    mpf_t euler, fat, temp;
+    mpf_set_default_prec(1000000);
 
-    mpf_init2(e, 100000);
-    mpf_init2(temp, 100000);
-    mpf_init2(fat, 100000);
+    mpf_init_set_ui(euler, 1);
+    mpf_init_set_ui(fat, 1);
+    mpf_init_set_ui(temp, 1);
 
-    mpf_set_d(e, 1.0);
-    mpf_set_d(temp, 1.0);
-    mpf_set_d(fat, 1.0);
-
-    for(int i = 1; i <= 1000000; i++)
+    for(int i = 1; i < 10000; i++)
     {
-        mpf_mul_ui(fat, fat, i);
         mpf_set_ui(temp, 1);
-        mpf_div(temp, temp, fat);
-        mpf_add(e, e, temp);
-    }
-
-    gmp_printf("%.*Ff", 10000, e);
-    return 0;
-}
-
-=======
-    mpf_set_default_prec(100000);
-    mpf_t e, temp, fat;
-
-    mpf_init2(e);
-    mpf_init2(temp);
-    mpf_init2(fat);
-
-    mpf_init2(e, 10000);
-    mpf_init2(temp, 10000);
-    mpf_init2(fat, 10000);
-
-    mpf_set_d(e, 1.0);
-    mpf_set_ui(temp, 1);
-    mpf_set_ui(fat, 1);
-
-    for(int i = 1; i <=20000; i++)
-    {
         mpf_mul_ui(fat, fat, i);
-        mpf_div(temp, 1, fat);
-        mpf_add(e, e, temp);
+        mpf_div(temp, temp, fat);
+        mpf_add(euler, euler, temp);
     }
 
-    gmp_printf("%.*Ff", 20000, e);
+    gmp_printf("Euler = %*.35655Ff\n", 0, euler);
+
+    mpf_clear(euler);
+    mpf_clear(fat);
+    mpf_clear(temp);
+
     return 0;
 }
->>>>>>> bcd51a863a5ce04d2a24eb60a9c2a89230952d60
--- Projeto/Proj2.c
+++ Projeto/Proj2.c
@@ -1,25 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <gmp.h>
+#include <omp.h>
 
-int main(void) 
+void factorial_list(mpf_t *list, int n);
+
+int main()
 {
-    mpf_t e, temp, fat;
+    int n = 10000;
+    mpf_t euler, fat, temp;
+    mpf_t *listfat = malloc((n + 1) * sizeof(mpf_t));
+    mpf_set_default_prec(1000000);
 
-    mpf_init2(e, 10000);
-    mpf_init2(temp, 10000);
-    mpf_init2(fat, 10000);
+    mpf_init_set_ui(euler, 1);
+    mpf_init_set_ui(fat, 1);
+    mpf_init_set_ui(temp, 1);
 
-    mpf_set_d(e, 1.0);
-    mpf_set_ui(temp, 1);
-    mpf_set_ui(fat, 1);
+    factorial_list(listfat, n);
+    gmp_printf("Lista Pronta\n");
 
-    for(int i = 1; i <= 10000; i++)
+    #pragma omp parallel for reduction(+:euler) private(fat,temp)
+    for(int i = 1; i <= n; i++)
     {
-        mpf_mul_ui(fat, fat, i);
-        mpf_div(temp, temp, fat);
-        mpf_add(e, e, temp);
+        mpf_set_ui(temp, 1);
+
+        mpf_div(temp, temp, listfat[i]);
+        mpf_add(euler, euler, temp);
+
     }
 
-    gmp_printf("%.*Ff", 10000, e);
+    gmp_printf("Euler = %.35655Ff\n", euler);
+
+    mpf_clear(euler);
+    mpf_clear(fat);
+    mpf_clear(temp);
+
+    for (int i = 0; i <= n; i++) {
+        mpf_clear(listfat[i]);
+    }
+    free(listfat);
+
     return 0;
+}
+
+void factorial_list(mpf_t *list, int n)
+{
+    mpf_t temp;
+    mpf_init(temp);
+
+    for (int i = 0; i <= n; i++) {
+        mpf_init(list[i]);
+        mpf_set_ui(temp, i);
+        if (i > 0) {
+            mpf_mul(list[i], list[i - 1], temp);
+        } else {
+            mpf_set_ui(list[i], 1);
+        }
+    }
+
+    mpf_clear(temp);
 }
